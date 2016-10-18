e8ed8b1c41120a15378b0b7498f7e9d109328efa
Ryan Liu <azhai@126.com>
2016-10-18 18:48:48 +0800

    y2016

---
+++ archives/configs.php
@@ -0,0 +1,19 @@
+<?php
+return [
+    'debug' => true,
+    'route_key' => '',
+    'title' => '阿债的技术存档',
+    'tagline' => '',
+    'reading' => '开始阅读文档',
+    'cover_image' => 'img/cover.png',
+    'author' => 'Ryan Liu',
+    'memory_limit' => '256M',
+    'cache_ext' => '.yml',
+    #pages仓库
+    'repo_url' => 'https://git.coding.net/azhai/azhai.git',
+    'repo_branch' => 'coding-pages',
+    'links' => [
+        'Coding仓库' => 'https://coding.net/u/azhai/p/docx/git',
+        'Todaymade出品Daux.io' => 'http://todaymade.com',
+    ],
+];
